f81cfd5f70847b0e24a0b5b72908e350d52eff2a
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-09-09 17:00:22 +0530

    conditional statments in typescript

---
+++ conditionalstatments.js
@@ -0,0 +1,18 @@
+"use strict";
+//Conditional statment in ts
+//Exercise-1-check the number is positive or negative
+let num_1 = 1;
+if (num_1 > 0) {
+    console.log('Number is positve');
+}
+else {
+    console.log('Number is negative');
+}
+//Exercise-2-check the number is odd or even
+let num_2 = 2;
+if (num_2 % 2 === 0) {
+    console.log('Number is even');
+}
+else {
+    console.log('Number is odd');
+}
