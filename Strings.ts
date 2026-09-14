050fa376276505a022d845ecd00a9c04eb152d85
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-09-14 16:44:35 +0530

    strings in typescript

---
+++ Strings.ts
@@ -0,0 +1,5 @@
+//Strings in typescript
+//Exercise-1-count the character of string
+let userName: string = 'Nikhil';
+let countOfString: number = userName.split('').length;
+console.log(`Total Number of Character in String:${countOfString}`)
---
+++ strings.js
@@ -0,0 +1,6 @@
+"use strict";
+//Strings in typescript
+//Exercise-1-count the character of string
+let userName = 'Nikhil';
+let countOfString = userName.split('').length;
+console.log(`Total Number of Character in String:${countOfString}`);
