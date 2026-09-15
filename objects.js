fba595928bbb7d53fc44333b13c7070035c2fc11
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-09-15 17:35:40 +0530

    objects in typescript

--- objects.js
+++ objects.js
@@ -31,3 +31,26 @@ const cityOne = userOne.company?.address.city;
 console.log('Acessing the City from Nested object:', cityOne);
 const personName = userOne.name;
 console.log('Accessing the Person Name from Nested Object:', personName);
+//Exercise-4-loop through the object
+let movies = {
+    movieOne: 'Harry-Potter',
+    movieTwo: 'Avengers',
+    movieThree: 'Spider-Man',
+    movieFour: 'John Wick'
+};
+console.log('Loop through an object');
+for (const [key, value] of Object.entries(movies)) {
+    console.log(`${key}:${value}`);
+}
+//Exercise-5-Merge Two object
+let moviesTwo = {
+    moviesFive: 'Lord of the Ring',
+    moviesSix: 'SpiderWick Chronicles',
+    movieEight: 'Golden Compass',
+    moviesNine: 'American-pie',
+    moviesTen: 'VoiceMail for Issabella'
+};
+console.log('Merging Two Movies Object');
+let allMovie = { ...movies, ...moviesTwo };
+console.log('All Movies Object');
+console.log(allMovie);
