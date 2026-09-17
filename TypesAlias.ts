22e3b9e0ac778ddbf84db985e9796a583eea800c
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-09-17 16:53:48 +0530

    TypesAlias Exercise for beginner

--- TypesAlias.ts
+++ TypesAlias.ts
@@ -30,3 +30,63 @@ const point: Point3D = {
 
 console.log('Type Aliases Example: Point');
 console.log(point);
+
+//Exercise-1-Create a userRole type
+type userRole = string;
+let userRole: userRole = 'Developer';
+console.log('UserRole:', userRole)
+console.log(`Typeof UserRole is:${typeof userRole}`);
+
+//Exercise-2-create product status type
+type productStatus = boolean | string;
+let productstatus: productStatus = true ? 'Available' : 'Not-Available';
+console.log('Product Stauts:',productstatus);
+console.log(`Type of productstatus is:${typeof productstatus}`);
+
+//Exercise-3-Ftech api with response type
+// 1. This is our "Plate". It tells the computer exactly what a User looks like.
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  company: {
+    name: string;
+  };
+}
+
+interface ApiResponse<T> {
+  status: 'success' | 'error';
+  message: string;
+  data: T; 
+}
+
+async function fetchUsersFromInternet(): Promise<ApiResponse<User[]>> {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const usersList: User[] = await response.json();
+    return {
+      status: 'success',
+      message: 'Yay! We got the users!',
+      data: usersList
+    };
+  } catch (error) {
+    return {
+      status: 'error',
+      message: 'Oh no, something went wrong!',
+        data: []
+    };
+  }
+}
+
+async function startApp() {
+  const result = await fetchUsersFromInternet();
+  if (result.status === 'success') {
+    console.log(result.message);
+    result.data.forEach(user => {
+      console.log(`👋 Hi ${user.name}! You work at "${user.company.name}".`);
+    });
+  } else {
+    console.log(result.message);
+  }
+}
+startApp();
--- typesalias.js
+++ typesalias.js
@@ -17,3 +17,40 @@ const point = {
 };
 console.log('Type Aliases Example: Point');
 console.log(point);
+let userRole = 'Developer';
+console.log('UserRole:', userRole);
+console.log(`Typeof UserRole is:${typeof userRole}`);
+let productstatus = true ? 'Available' : 'Not-Available';
+console.log('Product Stauts:', productstatus);
+console.log(`Type of productstatus is:${typeof productstatus}`);
+async function fetchUsersFromInternet() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const usersList = await response.json();
+        return {
+            status: 'success',
+            message: 'Yay! We got the users!',
+            data: usersList
+        };
+    }
+    catch (error) {
+        return {
+            status: 'error',
+            message: 'Oh no, something went wrong!',
+            data: []
+        };
+    }
+}
+async function startApp() {
+    const result = await fetchUsersFromInternet();
+    if (result.status === 'success') {
+        console.log(result.message);
+        result.data.forEach(user => {
+            console.log(`👋 Hi ${user.name}! You work at "${user.company.name}".`);
+        });
+    }
+    else {
+        console.log(result.message);
+    }
+}
+startApp();
